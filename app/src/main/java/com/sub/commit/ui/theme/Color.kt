be99550ff878cd714a310dadbe93194ccd9636a7
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2024-09-19 11:26:30 +0900

    feat: Base Font, TextStyle 추가

--- app/src/main/java/com/sub/commit/ui/theme/Color.kt
+++ app/src/main/java/com/sub/commit/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.sub.commit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+object ColorTokens {
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+    val MainColor = Color(0xFF2B2B2B)
+
+    val Black = Color(0xFF171717)
+    val White = Color.White
+
+    val Gray100 = Color(0xFFF5F5F5)
+    val Gray200 = Color(0xFFE9E9E9)
+    val Gray300 = Color(0xFFDDDDDD)
+    val Gray400 = Color(0xFFBCBCBC)
+    val Gray500 = Color(0xFFADADAD)
+    val Gray600 = Color(0xFF666666)
+    val Gray700 = Color(0xFF373737)
+
+    val Warning = Color(0xFFF54645)
+    val Success = Color(0xFF3AD32C)
+
+}
--- app/src/main/java/com/sub/commit/ui/theme/Type.kt
+++ app/src/main/java/com/sub/commit/ui/theme/Type.kt
@@ -1,34 +1,33 @@
 package com.sub.commit.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.sub.myapplication.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+object FontSizeTokens {
+    val HeadingLg = 24.sp
+    val HeadingMd = 20.sp
+    val HeadingSmall = 16.sp
+    val BodyMd = 16.sp
+    val BodySmall = 14.sp
+    val BodyXSmall = 12.sp
+    val BodyXXSmall = 12.sp
+}
+
+object TypeToken {
+
+    private val baseTextStyle = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.pretendard_bold, FontWeight.Bold),
+            Font(R.font.pretendard_medium, FontWeight.Medium),
+            Font(R.font.pretendard_regular, FontWeight.Normal),
+            Font(R.font.pretendard_light, FontWeight.Light),
+            Font(R.font.pretendard_thin, FontWeight.Thin)
+        )
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
-)
+
+
+}
