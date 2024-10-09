2499ffd7a1ffde038d3ff2f7cb1d4d450be5be31
yeomjiseok <ygs01033@naver.com>
2024-10-09 16:37:23 +0900

    feat: LoginViewModel 추가

--- app/src/main/java/com/sub/commit/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/sub/commit/ui/login/LoginViewModel.kt
@@ -1,13 +1,27 @@
 package com.sub.commit.ui.login
 
-import androidx.lifecycle.ViewModel
+import com.sub.commit.common.viewmodel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor (
+class LoginViewModel @Inject constructor() :
+    BaseViewModel<LoginContract.LoginState, LoginContract.LoginEvent, LoginContract.LoginSideEffect>() {
 
-): ViewModel() {
+    override fun createInitialState(): LoginContract.LoginState {
+        return LoginContract.LoginState()
+    }
 
+    override suspend fun handleEvent(event: LoginContract.LoginEvent) {
+        when (event) {
+            is LoginContract.LoginEvent.ClickLoginButton -> {
+                // updateNumber(operator = +1, toastMsg = "add")
+            }
+        }
+    }
 
+    /*   private fun updateNumber(operator: Int, toastMsg: String) {
+           reduce { copy(number = state.value.number + operator) }
+           postSideEffect(SignUpContract.SignUpSideEffect.ShowToast(toastMsg))
+       }*/
 }
